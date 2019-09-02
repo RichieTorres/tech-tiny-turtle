fa6043819b1399a14ab605f3bc2c3cbd8476e348
Ricardo Torres <ricardo.torres@endava.com>
2019-09-02 10:38:34 -0500

    nodejs-lts

--- packages/main.py
+++ packages/main.py
@@ -1,9 +1,11 @@
-import os
 from pathlib import Path
-import json
-import urllib.request
+import shutil
 import hashlib
+import json
+import os
 import tarfile
+import urllib.request
+from shutil import copytree, rmtree
 
 DATA_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
 
@@ -11,21 +13,7 @@ HOME_DIR = str(Path.home())
 CACHE_FOLDER = os.path.join(HOME_DIR, '.cache', 'ttt')
 OPT_FOLDER = os.path.join(HOME_DIR, '.local', 'opt')
 ENTRY_FOLDER = os.path.join(HOME_DIR, '.local', 'share', 'applications')
-
-
-def download_file(url, original_md5sum):
-    # Download the file from `url` and save it locally under `file_name`:
-    file_name = os.path.join(CACHE_FOLDER, os.path.basename(url))
-
-    md5sum = None
-    if os.path.exists(file_name):
-        md5sum = hashlib.md5(open(file_name, 'rb').read()).hexdigest()
-
-    if not original_md5sum == md5sum:
-        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
-            data = response.read()  # a `bytes` object
-            out_file.write(data)
-    return file_name
+BIN_FOLDER = os.path.join(HOME_DIR, '.local', 'bin')
 
 
 def install(p):
@@ -51,12 +39,56 @@ def install(p):
     print(f'Downloading {url}')
     file_name = download_file(url, md5sum)
 
-    tar = tarfile.open(file_name)
-    tar.extractall(os.path.join(OPT_FOLDER, p))
-    tar.close()
+    extract_file(file_name, p, data['extract_with_folder'])
+    link_bin_folder(p)
+
+
+def download_file(url, original_md5sum):
+    # Download the file from `url` and save it locally under `file_name`:
+    file_name = os.path.join(CACHE_FOLDER, os.path.basename(url))
+
+    md5sum = None
+    if os.path.exists(file_name):
+        md5sum = hashlib.md5(open(file_name, 'rb').read()).hexdigest()
+
+    if original_md5sum == md5sum:
+        print('Found in cache')
+    else:
+        with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+            data = response.read()  # a `bytes` object
+            out_file.write(data)
+    return file_name
 
 
 def create_desktop_entry(package_name, entry):
     package_folder = os.path.join(OPT_FOLDER, package_name)
     with open(os.path.join(ENTRY_FOLDER, f'{package_name}.desktop'), 'w') as out_file:
         out_file.write(entry.format(OPT_FOLDER=package_folder))
+
+
+def extract_file(file_name, package_name, extract_with_folder=False):
+    final_folder = os.path.join(OPT_FOLDER, package_name)
+    tmp_folder = os.path.join('/tmp/', package_name)
+    dest = tmp_folder if extract_with_folder else final_folder
+
+    if os.path.isdir(final_folder):
+        rmtree(final_folder)
+    if os.path.isdir(tmp_folder):
+        rmtree(tmp_folder)
+
+    tar = tarfile.open(file_name)
+    tar.extractall(dest)
+    tar.close()
+
+    if extract_with_folder:
+        inner_folder = None
+        for r in os.listdir(tmp_folder):
+            inner_folder = os.path.join(tmp_folder, r)
+        shutil.move(inner_folder, final_folder)
+
+
+def link_bin_folder(package_name):
+    package_bin_folder = os.path.join(OPT_FOLDER, package_name, 'bin')
+    if os.path.isdir(package_bin_folder):
+        for f in os.listdir(package_bin_folder):
+            os.symlink( os.path.join(package_bin_folder, f), os.path.join(BIN_FOLDER, f))
